f3b1e2e43f09a18089a0b6edb9314c7ec8a8ecd1
Om Prakash <prakash.oprime.1995@gmail.com>
2023-01-04 12:19:53 +0530

    added question also

--- Level-1/Recursion and Backtracking/Recursion in Arrays/first_index.py
+++ Level-1/Recursion and Backtracking/Recursion in Arrays/first_index.py
@@ -1,3 +1,16 @@
+'''
+1. You are given a number n, representing the count of elements.
+2. You are given n numbers.
+3. You are given a number x. 
+4. You are required to find the first index at which x occurs in array a.
+5. If x exists in array, print the first index where it is found otherwise print -1.
+
+Note -> The online judge can't force you to write the function recursively but that is
+what the spirit of question is. Write recursive and not iterative logic. The purpose 
+of the question is to aid learning recursion and not test you.
+
+'''
+
 # below method have logic to implement first index of an array
 def Firstindex(arr, idx, x, n):
     # stopping the recursion at last index of array
