77422af948a355d746e98ab3a33a6b96b6839776
Om Prakash <prakash.oprime.1995@gmail.com>
2023-03-02 12:35:11 +0530

    dp3

--- Level-1/Dynamic_Programming/Dynamic_Programming-And-Greedy/climb_stairs_dp.py
+++ Level-1/Dynamic_Programming/Dynamic_Programming-And-Greedy/climb_stairs_dp.py
@@ -10,27 +10,30 @@ staircase.
 
 """
 
+
 # By using tabulation.
 
 def fun(n, dp):
     # write code here
-    dp[0]=1
-    for i in range(1,n+1):
-        if i ==1:
-            dp[i]=dp[i-1]
-        elif i==2:
-            dp[i]=dp[i-1]+dp[i-2]
+    dp[0] = 1
+    for i in range(1, n + 1):
+        if i == 1:
+            dp[i] = dp[i - 1]
+        elif i == 2:
+            dp[i] = dp[i - 1] + dp[i - 2]
         else:
-            dp[i]=dp[i-1]+dp[i-2]+dp[i-3]
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
     return dp[n]
-    
+
+
 # driver code
 def main():
     n = int(input())
     dp = [0] * (n + 1)
     print(fun(n, dp))
-    
+
+
 if __name__ == "__main__":
     main()
-  
-  # checked with input ===3 output ==7
+
+# checked with input ===3 output ==7
